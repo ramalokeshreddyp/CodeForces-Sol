9906d19f2809433b0de09f1e2870fda58f01d431
Rama Lokesh Reddy Penumallu <153359335+ramalokeshreddyp@users.noreply.github.com>
2025-06-29 07:59:57 +0530

    Add Trippi Troppi [A] from Codeforces

---
+++ 2094/A - Trippi Troppi/solution.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string a, b, c;
+        cin >> a >> b >> c;
+        cout << a[0] << b[0] << c[0] << endl;
+    }
+    return 0;
+}
